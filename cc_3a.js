a0bb21505728dcaf8999695d1972f6acb1a0548c
shanebrown77 <Bds1@usf.edu>
2025-09-14 22:15:30 -0400

    inital commit

---
+++ cc_3a.js
@@ -0,0 +1,30 @@
+// coding challenge 3a
+
+let customers = [
+   
+    {name:"Mary Martyr", email:"marymartyr77@gmail.com", purchases:["bible", "handkerchief","tuxedo"]           },
+    {name:"James Ham", email:"jamesHAM555@gmail.com", purchases:["ham", "salami", "turkey"]                     },
+    {name:"Sarah Pinecone", email:"SarahPinecone9@gmail.com", purchases:["tomato seeds","sapling","fertilizer"] },
+
+
+
+];
+
+// adding/removal of data
+
+let newCustomer = {name: "David Payne", email:"DavidTHEPayne47@gmail.com", purchases:["dumbell", "water bottle", "comic book"]}
+    
+    customers.push(newCustomer);
+
+    customers.shift();
+
+// updating customer info
+    
+    customers[1].email = "SarahlikesPie99@gmail.com"
+
+    customers[2].purchases.push("tank top")
+
+
+
+
+console.log(customers)
